4a2bb927c4ef2def60e414a52603c10192995569
PedroGC <99632591+Pedrogcost@users.noreply.github.com>
2022-02-19 01:43:33 -0300

    formatting_text

---
+++ Exercícios Python/formatting_text.py
@@ -0,0 +1,11 @@
+from re import U
+
+
+message="\thi, this is my second day in the python course"
+print(message)
+first_name="pedro"
+last_name="goncalves"
+full_name=first_name+" "+last_name
+message="Hi,My name is " + full_name.title() + ", and i am very happy to learn python!\n\tI love Python!\n\tBye, Thanks!"
+print (message)
+input()
